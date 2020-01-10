6c23a80b6ce433be191f48ee2ff0326af0f6bc88
Alex Boswell <Aboswell@sfu.ca>
2020-01-09 16:16:20 -0800

    change main window to NavigationWindow

--- WPF_tutorial_expense_tracker/MainWindow.xaml.cs
+++ WPF_tutorial_expense_tracker/MainWindow.xaml.cs
@@ -18,7 +18,7 @@ namespace WPF_tutorial_expense_tracker
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : NavigationWindow
     {
         public MainWindow()
         {
